5f6344c2e12b92e220de4a95426d2de7716f753c
ruhul1845 <rohul4316@gmail.com>
2026-05-01 19:45:55 +0600

    fetching problem in deploying in vercel

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: "https://skillsphere-drab.vercel.app/"
+    baseURL: process.env.BETTER_AUTH_URL
 });
 
 export const { signIn, signUp, signOut, updateUser, useSession } = authClient;
